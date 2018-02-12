abedfce48008e62a3b2f60f5e62cbd8231a4d31b
Dustin Brickwood <dustinbrickwood204@gmail.com>
2018-02-11 21:51:33 -0500

    UI work

--- atomstats/src/component/SideNav.js
+++ atomstats/src/component/SideNav.js
@@ -1,37 +1,31 @@
 import React from "react";
-import SideNav from './SideNav.css';
-import cosmosLogo from '../assets/images/cosmosLogo.png';
+import SideNav from "./SideNav.css";
+import cosmosLogo from "../assets/images/cosmosLogo.png";
 
 const sideNav = props => {
   return (
-    <div class="container-fluid">
-      <div class="row-fluid">
-        <div class="col-sm-3 col-md-2 sidebar">
-          <img src={cosmosLogo} className="cosmos-logo"  />
-          <ul class="nav nav-sidebar">
-            <li>
-              <a href="#" class="Active">
-                ChoiceA
-              </a>
+    <div className="container-fluid">
+      <div className="row-fluid">
+        <div className="col-sm-3 col-md-2 sidebar">
+          <img src={cosmosLogo} className="cosmos-logo" />
+          <ul className="nav nav-sidebar side-items">
+            <li className="side-item">
+              <span className="side-name">Atom % of Network </span>
             </li>
-            <li>
-              <a href="#">ChoiceB</a>
+            <li className="side-item">
+              <span className="side-name">Delegate Commission</span>
             </li>
-            <li>
-              <a href="#" class="Active">
-                ChoiceA
-              </a>
+            <li className="side-item">
+              <span className="side-name">Atoms Delegated</span>
             </li>
-            <li>
-              <a href="#">ChoiceB</a>
+            <li className="side-item">
+              <span className="side-name">Atoms Self-Bonded</span>
             </li>
-            <li>
-              <a href="#" class="Active">
-                ChoiceA
-              </a>
+            <li className="side-item">
+              <span className="side-name">ChoiceA</span>
             </li>
-            <li>
-              <a href="#">ChoiceB</a>
+            <li className="side-item">
+              <span className="side-name">ChoiceA</span>
             </li>
           </ul>
         </div>
